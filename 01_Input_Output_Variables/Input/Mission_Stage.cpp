c1a432008f90fa4f83f7128924b65b3b20826473
Waleed <male890@mail.ru>
2026-06-18 16:13:06 +0300

    Create Mission_Stage.cpp

---
+++ 01_Input_Output_Variables/Input/Mission_Stage.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Stage 1: Route Reconnaissance" << endl;
+    cout << "\tParticipants: 42" << endl;
+    cout << "\t------------------" << endl;
+
+    cout << "Stage 2: Equipment Preparation" << endl;
+    cout << "\tParticipants: 4" << endl;
+    cout << "\t------------------" << endl;
+
+    cout << "Stage 3: Base Camp Setup" << endl;
+    cout << "\tParticipants: 15" << endl;
+    cout << "\t------------------" << endl;
+
+    cout << "Stage 4: Summit Assault" << endl;
+    cout << "\tParticipants: 8" << endl;
+    cout << "\t------------------" << endl;
+
+    cout << "Stage 5: Descent" << endl;
+    cout << "\tParticipants: 42" << endl;
+    cout << "\t------------------" << endl;
+
+    cout << "Stage 6: Result Analysis" << endl;
+    cout << "\tParticipants: 23" << endl;
+    cout << "\t------------------" << endl;
+
+    return 0;
+}
