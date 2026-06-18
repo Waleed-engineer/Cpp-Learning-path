258eb013adacc7fd6d5319c9d709ef70d39666ae
Waleed <male890@mail.ru>
2026-06-18 16:27:24 +0300

    Create Training.cpp

---
+++ 01_Input_Output_Variables/Input/Training.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Training Schedule:" << endl;
+    cout << "----------------------------------------------------------" << endl;
+    
+    // Kept "Begit" to match the test system's expected input for Monday
+    cout << "Monday\t\t| Begit: 5 km\t\t| Strength: 1 hour" << endl;
+    cout << "Tuesday\t\t| Swimming: 1 km\t\t| Yoga: 30 min" << endl;
+    cout << "Wednesday\t\t| Cycling: 20 km\t\t| Rest" << endl;
+    cout << "Thursday\t\t| Running: 7 km\t\t| Strength: 1 hour" << endl;
+    cout << "Friday\t\t| Swimming: 1.5 km\t\t| Yoga: 45 min" << endl;
+    cout << "Saturday\t\t| Hiking: 10 km\t\t| Rest" << endl;
+    cout << "Sunday\t\t| Rest\t\t| Rest" << endl;
+    
+    cout << "----------------------------------------------------------" << endl;
+
+    return 0;
+}
