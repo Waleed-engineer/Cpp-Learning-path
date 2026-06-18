259bd26b85470b748818213125fdd69f4c855e60
Waleed <male890@mail.ru>
2026-06-18 15:02:05 +0300

    Add Cosmonaut.cpp for displaying cosmonaut info

---
+++ 01_Input_Output_Variables/Input/Cosmonaut.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "+----------------------------------------+" << endl;
+    cout << "|        *** COSMONAUT FILE *** |" << endl;
+    cout << "+----------------------------------------+" << endl;
+    cout << "| CALLSIGN: Kedr                         |" << endl;
+    cout << "|                                        |" << endl;
+    cout << "| LAST NAME: Gagarin                     |" << endl;
+    cout << "| FIRST NAME: Yuri                       |" << endl;
+    cout << "| PATRONYMIC: Alekseyevich               |" << endl;
+    cout << "|                                        |" << endl;
+    cout << "| BIRTHPLACE: Klushino                   |" << endl;
+    cout << "| AGE: 34                                |" << endl;
+    cout << "+----------------------------------------+" << endl;
+    cout << "|           MISSION: VOSTOK-1            |" << endl;
+    cout << "|                / \\                    |" << endl;
+    cout << "|               / _ \\                   |" << endl;
+    cout << "|              | / \\ |                  |" << endl;
+    cout << "|              | | | |                  |" << endl;
+    cout << "|              | | | |                  |" << endl;
+    cout << "|             /| | | |\\                 |" << endl;
+    cout << "|            / | | | | \\                |" << endl;
+    cout << "|           /  '---'  \\                 |" << endl;
+    cout << "|           '---------'                  |" << endl;
+    cout << "+----------------------------------------+" << endl;    
+    return 0;
+}
