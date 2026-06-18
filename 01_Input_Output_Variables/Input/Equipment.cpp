964aa04cc49dd0e1d0e646957001bc454d584e59
Waleed <male890@mail.ru>
2026-06-18 16:17:45 +0300

    list that displays  Equipment

---
+++ 01_Input_Output_Variables/Input/Equipment.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Equipment List:" << endl;
+    cout << "----------------------------" << endl;
+    
+    cout << "1. Backpack\t| 1 pc." << endl;
+    cout << "2. Tent\t\t| 1 pc." << endl;
+    cout << "3. Sleeping bag\t| 1 pc." << endl;
+    cout << "4. Carabiner\t| 10 pcs." << endl;
+    cout << "5. Rope\t\t| 2 pcs." << endl;
+    cout << "6. Flashlight\t| 1 pc." << endl;
+    
+    cout << "----------------------------" << endl;
+
+    return 0;
+}
