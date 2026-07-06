26c75b2138fab1144ff12da6b47e8733597cbd38
Waleed <male890@mail.ru>
2026-07-06 14:49:31 +0300

    Create 04_PROJECT.cpp

---
+++ 03_Pointers_And_Memory_Management/04_PROJECT.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    char ch, inp;
+    std::cin >> inp;
+
+
+    int temp = *p_1;
+    *p_1 = *p_2;
+    *p_2 = temp;
+
+
+    std::cout << *p_ch << std::endl;
+    return 0;
+}
