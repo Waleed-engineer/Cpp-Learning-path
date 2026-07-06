5e5f379655090a617b859ff8c8f7607df6b9d8a7
Waleed <male890@mail.ru>
2026-07-06 15:25:49 +0300

    Add main function for input and square calculation
    Implement main function to read input and calculate squares.

---
+++ 03_Pointers_And_Memory_Management/05_PROJECT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    char ch, inp;
+    std::cin >> inp;
+
+    int z, x;
+    std::cin >> z;
+    std::cin >> x;
+    z = z * z;
+    x = x * x;
+    *p_1 = z;
+    *p_2 = x;
+
+    std::cout << *p_ch << std::endl;
+    return 0;
+}
